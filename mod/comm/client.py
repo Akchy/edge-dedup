501c317fb2b1b98754f55fc0a764137f1fd0152b
Akchy <akarshashok12@gmail.com>
2023-03-02 19:27:08 +0530

    Client Server Communication Done

--- mod/comm/client.py
+++ mod/comm/client.py
@@ -1,15 +1,37 @@
 import socket
+from datetime import datetime
 
-# Create a socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT-Out"
+SERVER = "127.0.0.1"
+ADDR = (SERVER, PORT)
 
-# Connect to the server
-s.connect(("192.168.1.100", 1234))
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
 
-# Open the file
-with open("file_to_send.txt", "rb") as f:
-    # Send the file
-    s.sendall(f.read())
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    val =client.recv(2048).decode(FORMAT)
+    now = datetime.now()
 
-# Close the connection
-s.close()
+    #current_time = now.strftime("%H:%M:%S")
+    print(f'{now}: {val}')
+    return val
+
+key_list = ['key','hello there user with Public Key']
+key_string = '-'.join(key_list)
+#file_tag_list = ['tag','12345346']
+file_tag_list = ['tag']
+val = send(key_string)
+file_tag_list.append(val)
+file_tag_string = '-'.join(file_tag_list)
+send(file_tag_string)
+
+send(DISCONNECT_MESSAGE)
--- mod/comm/server.py
+++ mod/comm/server.py
@@ -1,26 +1,71 @@
-import socket
+import socket 
+import threading
+from datetime import datetime
 
-# Create a socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+HEADER = 64
+PORT = 5050
+SERVER = '127.0.0.1'
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT-Out"
 
-# Bind the socket to an IP address and port
-s.bind(("192.168.1.100", 1234))
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+#lock = threading.Lock()
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            #lock.acquire()
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+            str_to_list = msg.split('-')
+            now = datetime.now()
 
-# Listen for incoming connections
-s.listen(5)
+            #current_time = now.strftime("%H:%M:%S")
+            print(f'{now}:  {str_to_list[1]}')
+            #command = str_to_list[0]
+            #val = str_to_list[1]
+            #check_command(command,val)
+            #print(f"[{addr}] {msg}")
+            conn.send("Msg received".encode(FORMAT))
+            #lock.release()
+    print('done')
+    conn.close()
 
-while True:
-    # Accept a connection
-    c, addr = s.accept()
-    print("Got connection from", addr)
 
-    # Receive the file
-    with open("received_file.txt", "wb") as f:
-        while True:
-            data = c.recv(1024)
-            if not data:
-                break
-            f.write(data)
+def check_command(argument,val):
+    match argument:
+        case 'rce':
+            get_rce(val)
+        case 'file_tag':
+            get_file_tag(val)
+        case 2:
+            return "two"
+        case default:
+            return "something"
+ 
 
-    # Close the connection
-    c.close()
+def get_rce(val):
+    print(f"Here's rce value from client: {val}")
+
+
+def get_file_tag(val):
+    print(f"Here's file_tag value from client: {val}")
+
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
+
+print("[STARTING] server is starting...")
+start()
