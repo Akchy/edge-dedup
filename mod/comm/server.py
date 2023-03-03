38765ab6909cc272153ff87862ab6796b929ecf7
Akchy <akchy26@gmail.com>
2023-03-03 14:59:36 +0530

    Created server socket communication

--- mod/comm/server.py
+++ mod/comm/server.py
@@ -4,58 +4,39 @@ from datetime import datetime
 
 HEADER = 64
 PORT = 5050
-SERVER = '127.0.0.1'
-ADDR = (SERVER, PORT)
+SERVER = socket.gethostname()
+ADDR = ('', PORT)
 FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT-Out"
+DISCONNECT_MESSAGE = "!DISCONNECT-OUT"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
-#lock = threading.Lock()
 def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected.")
     connected = True
     while connected:
         msg_length = conn.recv(HEADER).decode(FORMAT)
         if msg_length:
-            #lock.acquire()
             msg_length = int(msg_length)
             msg = conn.recv(msg_length).decode(FORMAT)
             if msg == DISCONNECT_MESSAGE:
                 connected = False
             str_to_list = msg.split('-')
             now = datetime.now()
-
-            #current_time = now.strftime("%H:%M:%S")
-            print(f'{now}:  {str_to_list[1]}')
-            #command = str_to_list[0]
-            #val = str_to_list[1]
-            #check_command(command,val)
-            #print(f"[{addr}] {msg}")
+            command = str_to_list[0]
+            val = str_to_list[1]
+            check_command(command,val)
             conn.send("Msg received".encode(FORMAT))
-            #lock.release()
-    print('done')
     conn.close()
 
 
 def check_command(argument,val):
     match argument:
-        case 'rce':
+        case 'tag':
             get_rce(val)
-        case 'file_tag':
-            get_file_tag(val)
-        case 2:
-            return "two"
-        case default:
-            return "something"
  
 
 def get_rce(val):
     print(f"Here's rce value from client: {val}")
-
-
-def get_file_tag(val):
-    print(f"Here's file_tag value from client: {val}")
 
 def start():
     server.listen()
@@ -64,8 +45,5 @@ def start():
         conn, addr = server.accept()
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
 
-
-print("[STARTING] server is starting...")
 start()
